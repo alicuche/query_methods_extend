ce0182fac75883b7ac57b3dd9cd50878a39df794
alicuche <alicuche@gmail.com>
2015-02-09 17:53:24 +0700

    has_many collection with foreign_key, class, polymophic

--- lib/query_methods_extend/association.rb
+++ lib/query_methods_extend/association.rb
@@ -13,10 +13,32 @@ module QueryMethodsExtend
         association = self.try(:reflect_on_association, method_name)
         result = false
         if association && association.try(:macro) == :has_many
-          model_reflect = association.inverse_of
-          model = model_reflect.active_record
-          query = self.select(association.primary_key_column.name)
-          result = model.where("#{model.table_name}.#{model_reflect.foreign_key.to_sym} IN (#{query.to_sql})")
+          options = association.options
+
+          if options[:through]
+            @check_association_exist_flag = false
+            return self.all.send(options[:through]).send(method_name)
+          end
+
+          query = self.select(association.active_record.primary_key)
+
+          if options[:as] && !association.inverse_of && association.klass
+            polymorphic_as = association.klass.reflect_on_association(options[:as])
+            if polymorphic_as.polymorphic? && association.active_record
+              polymorphic_model = polymorphic_as.active_record
+              model = association.active_record
+              @check_association_exist_flag = false
+              return polymorphic_as.active_record
+                .where("#{polymorphic_model.table_name}.#{polymorphic_as.foreign_type}".to_sym => model.name)
+                .where("#{polymorphic_model.table_name}.#{polymorphic_as.foreign_key}".to_sym => query)
+            end
+          end
+
+          model = association.options[:class] || association.inverse_of.active_record
+          foreign_key = association.options[:foreign_key] || association.inverse_of.foreign_key
+
+          result = model.where("#{model.table_name}.#{foreign_key.to_s}".to_sym => query)
+
         end
 
         @check_association_exist_flag = false
